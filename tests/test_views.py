125f0d1e1326fd1e64c4378466c6248b9955bd88
Long Nguyen <wdragon93@yahoo.com>
2023-04-22 23:22:51 +1000

    Add unit test for menu view

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,10 +1,14 @@
 from django.test import TestCase
 from restaurant.models import Menu
+from django.core import serializers
 
 class MenuViewTest(TestCase):
     def setup(self):
-        pass
+        self.client = Client()
+        Menu.objects.create(title='IceCream', price=80, inventory=100)
+        Menu.objects.create(title='IceCream2', price=82, inventory=102)
 
     def test_getall(self):
-        item = Menu.objects.create(title="IceCream", price=80, inventory=100)
-        self.assertEqual(str(item), "IceCream : 80")
+        response = self.client.get('/restaurant/menu/')
+        expected = serializers.serialize('json', Menu.objects.all())
+        self.assertJSONEqual(response.content, expected)
